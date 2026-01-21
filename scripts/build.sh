aca2fb947e93382529059bcfe2f535e9735d9745
Mateo <mateobinance1@gmail.com>
2026-01-21 20:08:24 -0300

    upgrade ci pipelinne

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,5 @@ set -e
 
 echo "building container..."
 docker compose up --build -d
+echo "Container built successfully!"
+exit 0
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 set -e
-
+echo "Starting container..."
 docker compose up 
+echo "Container started successfully!"
+exit 0
